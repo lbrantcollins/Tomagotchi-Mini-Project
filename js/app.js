92f665d71764b0fcf534df0b5b870ae2f4bb62bd
lindabcollins <lindabcollins@gmail.com>
2019-07-02 15:32:04 -0500

    instantiated Tomagotchi class

--- js/app.js
+++ js/app.js
@@ -3,9 +3,20 @@ console.log("Are you ready to own a Tomagotchi?");
 class Tomagotchi {
 	constructor(name) {
 		this.name = name;
-		this.age = age;
+		this.age = 0;
 		this.hunger = 10;
 		this.sleepiness = 10;
 		this.boredom = 10;
 	}
 }
+
+const game = {
+
+	createTomagotchi(name) {
+		const pet = new Tomagotchi(name);
+ 	console.log(pet);
+	}
+
+}
+
+game.createTomagotchi("tommy");
