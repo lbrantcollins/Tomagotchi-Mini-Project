481f6b1ad999ea8a2ae1efa5c766b41d14dcd555
lindabcollins <lindabcollins@gmail.com>
2019-07-02 15:17:33 -0500

    basic Tomagotchi class created

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log("Are you ready to own a Tomagotchi?");
+
+class Tomagotchi {
+	constructor(name) {
+		this.name = name;
+		this.age = age;
+		this.hunger = 10;
+		this.sleepiness = 10;
+		this.boredom = 10;
+	}
+}
