75b140f22f8761dc36296a9124b3f2ed73ca7e7e
lindabcollins <lindabcollins@gmail.com>
2019-07-03 13:57:23 -0500

    added event listener skeleton

--- js/app.js
+++ js/app.js
@@ -14,34 +14,35 @@ class Tomagotchi {
 		this.limit = 10;
 		this.ageLimit = 4;
 	}
+	turnOutLights = () => {
+		console.log("inside turnOutLights function");
+		// change CSS image/animation to dark background
+		// and dark, sleeping image of pet
+	}
+
+	feedTomagotchi() {
+		console.log("inside feedTomagotchi function");
+		// change CSS image/animation to hungry pet
+	}
+
+	playWithTomagotchi() {
+		console.log("inside feedTomagotchi function");
+		// change CSS image/animation to playing pet
+		
+	}
 }
 
 //**************************************************
 // Game object and methods
 //**************************************************
 const game {
-
-	createTomagotchi = (name) => {
+	//Tomagotchi: null,
+	createTomagotchi: function(name){
 		console.log("inside createTomagotchi function");
 		const pet = new Tomagotchi(name);
 	},
 
-	turnOutLights = (sleepiness) => {
-		console.log("inside turnOutLights function");
-		// change CSS image/animation to dark background
-		// and dark, sleeping image of pet
-	},
-
-	feedTomagotchi = (hunger) => {
-		console.log("inside feedTomagotchi function");
-		// change CSS image/animation to hungry pet
-	},
-
-	playWithTomagotchi = (boredom) => {
-		console.log("inside feedTomagotchi function");
-		// change CSS image/animation to playing pet
-		
-	}
+	
 
 }
 
@@ -50,8 +51,10 @@ game.createTomagotchi("tommy");
 //**************************************************
 // Event listeners
 //**************************************************
-('#feed-me').on('click', feedTomagotchi( HOW to pass "hunger" here?);
+$('#feed-me').on('click', (e) => {
+	game.Tomagotchi.feedTomagotchi();
+})
 
-('#play-with-me').on('click', playWithTomagotchi( HOW to pass "boredom" here?);
+('#play-with-me').on('click', game.Tomagotchi.playWithTomagotchi();
 
-('#turn-out-lights').on('click'), turnOutLights( HOW to pass "sleepiness" here?)
+('#turn-out-lights').on('click'), game.Tomagotchi.turnOutLights( HOW to pass "sleepiness" here?)
