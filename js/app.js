3b493c903edd278aa6df62c50db3c4941221faf6
lindabcollins <lindabcollins@gmail.com>
2019-07-03 13:02:18 -0500

    added js outline of methods and listeners for three actions: feed, play, sleep

--- js/app.js
+++ js/app.js
@@ -1,22 +1,57 @@
 console.log("Are you ready to own a Tomagotchi?");
 
+
+//**************************************************
+// Definition of Tomagotchi class
+//**************************************************
 class Tomagotchi {
 	constructor(name) {
 		this.name = name;
 		this.age = 0;
-		this.hunger = 10;
-		this.sleepiness = 10;
-		this.boredom = 10;
+		this.hunger = 0;
+		this.sleepiness = 0;
+		this.boredom = 0;
+		this.limit = 10;
+		this.ageLimit = 4;
 	}
 }
 
-const game = {
+//**************************************************
+// Game object and methods
+//**************************************************
+const game {
 
-	createTomagotchi(name) {
+	createTomagotchi = (name) => {
+		console.log("inside createTomagotchi function");
 		const pet = new Tomagotchi(name);
- 	console.log(pet);
+	},
+
+	turnOutLights = (sleepiness) => {
+		console.log("inside turnOutLights function");
+		// change CSS image/animation to dark background
+		// and dark, sleeping image of pet
+	},
+
+	feedTomagotchi = (hunger) => {
+		console.log("inside feedTomagotchi function");
+		// change CSS image/animation to hungry pet
+	},
+
+	playWithTomagotchi = (boredom) => {
+		console.log("inside feedTomagotchi function");
+		// change CSS image/animation to playing pet
+		
 	}
 
 }
 
 game.createTomagotchi("tommy");
+
+//**************************************************
+// Event listeners
+//**************************************************
+('#feed-me').on('click', feedTomagotchi( HOW to pass "hunger" here?);
+
+('#play-with-me').on('click', playWithTomagotchi( HOW to pass "boredom" here?);
+
+('#turn-out-lights').on('click'), turnOutLights( HOW to pass "sleepiness" here?)
