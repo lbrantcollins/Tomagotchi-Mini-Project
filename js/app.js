bab1f9f21523321966b2d85df017d7c6aec7ab6d
lindabcollins <lindabcollins@gmail.com>
2019-07-03 14:10:36 -0500

    fleshed out some more structure and methods

--- js/app.js
+++ js/app.js
@@ -14,16 +14,14 @@ class Tomagotchi {
 		this.limit = 10;
 		this.ageLimit = 4;
 	}
-	turnOutLights = () => {
-		console.log("inside turnOutLights function");
-		// change CSS image/animation to dark background
-		// and dark, sleeping image of pet
-	}
+
 
 	feedTomagotchi() {
 		console.log("inside feedTomagotchi function");
 		// change CSS image/animation to hungry pet
 	}
+
+	getHungrier(){}
 
 	playWithTomagotchi() {
 		console.log("inside feedTomagotchi function");
@@ -35,14 +33,27 @@ class Tomagotchi {
 //**************************************************
 // Game object and methods
 //**************************************************
-const game {
-	//Tomagotchi: null,
+const game = {
+
+	tomagotchi: null,
+	handle: null,
+	timeElapsed: 0,
 	createTomagotchi: function(name){
 		console.log("inside createTomagotchi function");
 		const pet = new Tomagotchi(name);
+		this.tomagotchi = pet;
 	},
 
-	
+	startTimer() {
+		// MDN timer -- returns a handle that can be used to stop timer
+		// setInterval -- increase time elapsed
+	},
+
+	turnOutLights() {
+		console.log("inside turnOutLights function");
+		// change CSS image/animation to dark background
+		// and dark, sleeping image of pet
+	}
 
 }
 
@@ -52,9 +63,14 @@ game.createTomagotchi("tommy");
 // Event listeners
 //**************************************************
 $('#feed-me').on('click', (e) => {
-	game.Tomagotchi.feedTomagotchi();
+	game.tomagotchi.feedTomagotchi();
 })
 
-('#play-with-me').on('click', game.Tomagotchi.playWithTomagotchi();
+$('#play-with-me').on('click', (e) => {
+	game.tomagotchi.playWithTomagotchi()
+});
 
-('#turn-out-lights').on('click'), game.Tomagotchi.turnOutLights( HOW to pass "sleepiness" here?)
+
+$('#turn-out-lights').on('click', (e) => {
+	games.turnOutLights()
+});
