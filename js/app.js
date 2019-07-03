a348a0cc035137cd1428ca665e58eea11f73457a
lindabcollins <lindabcollins@gmail.com>
2019-07-03 15:47:14 -0500

    added a working timer linked up to tomagotchi increments

--- js/app.js
+++ js/app.js
@@ -15,23 +15,44 @@ class Tomagotchi {
 		this.ageLimit = 4;
 	}
 
-	getSleepier() {
+	// Sleepiness actions:
+	//***********************
+	getSleepier () {
 		console.log("inside getSleepier function");
 		this.sleepiness++;
 	}
+
+	beRested () {
+		console.log("inside beRested");
+		this.hunger = max(0, this.hunger -  3);
+	}
 	
+	// Hunger actions:
+	//***********************
 	getHungrier () {
-		console.log("inside getHungier function");
+		console.log("inside getHungier");
 		this.hunger++;
 	}
 
+	beFed() {
+		console.log("inside beFed");
+		this.hunger = max(0, this.hunger -  3);
+	}
+
+	// Boredom actions:
+	//***********************
 	getMoreBored () {
-		console.log("inside getMoreBored function");
+		console.log("inside getMoreBored");
 		this.boredom++;
 	}
 
+	bePlayedWith() {
+		console.log("inside bePlayedWith");
+		this.boredom = max(0, this.boredom -  3);
+	}
+
 	growOlder () {
-		console.log("inside growOlder function");
+		console.log("inside growOlder");
 		this.age++;
 
 	}
@@ -45,70 +66,93 @@ const game = {
 	tomagotchi: null,
 	handle: null,
 	timeElapsed: 0,
+
 	createTomagotchi: function(name) {
 		console.log("inside createTomagotchi function");
 		this.tomagotchi = new Tomagotchi(name);
 	},
 
-	startTimer() {
-		// MDN timer -- returns a handle that can be used to stop timer
-		// setInterval -- increase time elapsed
-
+	// Animation is triggered by button push
+	//*******************************************
+	// Let the tomagotchi sleep
+	turnOutLights() {
+		console.log("inside turnOutLights function");
+		this.tomoagotchi.beRested();
+		// change CSS image/animation to dark background
+		// and dark, sleeping image of pet
 	},
-
+	// Feed the tomagotchi
 	feedTomagotchi() {
 		console.log("inside feedTomagotchi function");
+		this.tomoagotchi.beFed();
 		// change CSS image/animation to hungry pet
 	},
-
+	// play with the tomagotchi
 	playWithTomagotchi() {
-		console.log("inside feedTomagotchi function");
+		console.log("inside playWithTomagotchi function");
+		this.tomoagotchi.bePlayedWith();
 		// change CSS image/animation to playing pet
 		
 	},
 
-	turnOutLights() {
-		console.log("inside turnOutLights function");
-		// change CSS image/animation to dark background
-		// and dark, sleeping image of pet
-	},
+	
 
 	ageTomagotchi () {
 		console.log("inside ageTomagotchi function");
 		// change CSS image/animation increase size of pet
 	},
 
-	setTimer () {
+	startTimer() {
+		// MDN timer -- returns a handle that can be used to stop timer
+		// setInterval -- increase time elapsed
 
 	   const timer = setInterval( () => {
 	   	this.timeElapsed++;
-	   	if (timeElapse % 3 === 0) {
-	   		tomagotchi.getSleepier();
+	   	console.log(`time: ${this.timeElapsed}`);
+	   	// a failsafe to get out of the timer
+			if (this.timeElapsed >= 15) {
+				clearInterval(timer);
+				console.log("Thank goodness I put in a timer stopper!");
+			}
+
+	   	// $('#timer').text(`Time: ${this.time}s`);
+
+	   	// Sleepiness increase (slowest to change)
+	   	if (this.timeElapsed % 7 === 0) {
+	   		console.log("increasing sleep counter");
+	   		this.tomagotchi.getSleepier();
+	   	}
+	   	// Hunger increase
+	   	if (this.timeElapsed % 5 === 0) {
+	   		console.log("increasing hunger counter");
+	   		this.tomagotchi.getHungrier();
+	   	}
+	   	// Boredom increase (fastest to change)
+	   	if (this.timeElapsed % 3 === 0) {
+	   		console.log("increasing boredom counter");
+	   		this.tomagotchi.getMoreBored();
 	   	}
 
-			if ( 	(tomagotchi.sleepiness >= tomagotchi.limit)
-					||	(tomagotchi.sleepiness >= tomagotchi.limit)
-					||	(tomagotchi.sleepiness >= tomagotchi.limit)
-					||	(tomagotchi.age >= tomagotchi.agelimit)
+	   	// Check if tomagotchi has died or grown too old	
+			if ( 	(this.tomagotchi.sleepiness >= this.tomagotchi.limit)
+					||	(this.tomagotchi.sleepiness >= this.tomagotchi.limit)
+					||	(this.tomagotchi.sleepiness >= this.tomagotchi.limit)
+					||	(this.tomagotchi.age >= this.tomagotchi.agelimit)
 				) {
 
 				clearInterval(timer);
 				console.log("end of game");
 			}
-			$('#timer').text(`Time: ${this.time}s`);
+			
 
-			// a failsafe to get out of the timer
-			if (this.timeElapsed >= 10) {
-				clearInterval(timer);
-				console.log("Thank goodness I put in a timer stopper!");
-			}
-		}, 1000);
+			
+		}, 3000);
   	}
 
 }
 
 game.createTomagotchi("tommy");
-
+game.startTimer();
 
 //**************************************************
 // Event listeners
